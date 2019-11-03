62dfa1a5860c86d824ef5b2f52da38469d2508a6
boringem <madelyn.vagle@gmail.com>
2019-11-03 00:00:01 -0500

    updated spacing and title

--- sketch.js
+++ sketch.js
@@ -34,69 +34,61 @@ function preload() {
 
   function loadCanvas() {
     if(window.innerWidth > 992) {
-        line(200, 80, 1000, 80); //1
-        line(200, 110, 1000, 110); //2
-        line(200, 140, 1000, 140); //3
-        line(200, 170, 1000, 170); //4
-        line(200, 200, 1000, 200); //5
+        line(200, 70, 1000, 70); //1
+        line(200, 100, 1000, 100); //2
+        line(200, 130, 1000, 130); //3
+        line(200, 160, 1000, 160); //4
+        line(200, 190, 1000, 190); //5
   
-        stroke(0);
+        stroke(30, 30, 30);
         strokeWeight(2);
-        rect(250, 300, 100, 300); //1
-        rect(350, 300, 100, 300); //2
-        rect(450, 300, 100, 300); //3
-        rect(550, 300, 100, 300); //4
-        rect(650, 300, 100, 300); //5
-        rect(750, 300, 100, 300); //6
-        rect(850, 300, 100, 300); //7
+        rect(250, 250, 100, 250); //1
+        rect(350, 250, 100, 250); //2
+        rect(450, 250, 100, 250); //3
+        rect(550, 250, 100, 250); //4
+        rect(650, 250, 100, 250); //5
+        rect(750, 250, 100, 250); //6
+        rect(850, 250, 100, 250); //7
   
-        fill(0);
-        rect(325, 300, 50, 150); //1
-        rect(425, 300, 50, 150); //2
-        rect(625, 300, 50, 150); //3
-        rect(725, 300, 50, 150); //4
-        rect(825, 300, 50, 150); //5
+        fill(30, 30, 30);
+        rect(325, 250, 50, 120); //1
+        rect(425, 250, 50, 120); //2
+        rect(625, 250, 50, 120); //3
+        rect(725, 250, 50, 120); //4
+        rect(825, 250, 50, 120); //5
   
         noStroke();
         textSize(18);
-        fill(0, 102, 153);
-        text('C', 293, 550); //C
-        text('D', 393, 550); //D
-        text('E', 493, 550); //E
-        text('F', 593, 550); //F
-        text('G', 693, 550); //G
-        text('A', 793, 550); //A
-        text('B', 893, 550); //B
+        fill(60, 60, 60);
     }
 
     if(window.innerWidth < 992) {
-        line(25, 80, 350, 80); //1
-        line(25, 110, 350, 110); //2
-        line(25, 140, 350, 140); //3
-        line(25, 170, 350, 170); //4
-        line(25, 200, 350, 200); //5
+        line(25, 70, 350, 70); //1
+        line(25, 100, 350, 100); //2
+        line(25, 130, 350, 130); //3
+        line(25, 160, 350, 160); //4
+        line(25, 190, 350, 190); //5
   
-        //stroke(0);
         strokeWeight(2);
-        rect(15, 300, 45, 150); //1
-        rect(60, 300, 45, 150); //2
-        rect(105, 300, 45, 150); //3
-        rect(150, 300, 45, 150); //4
-        rect(195, 300, 45, 150); //5
-        rect(240, 300, 45, 150); //6
-        rect(285, 300, 45, 150); //7
+        rect(25, 250, 45, 140); //1
+        rect(70, 250, 45, 140); //2
+        rect(115, 250, 45, 140); //3
+        rect(160, 250, 45, 140); //4
+        rect(205, 250, 45, 140); //5
+        rect(250, 250, 45, 140); //6
+        rect(295, 250, 45, 140); //7
   
-        fill(0);
-        stroke(0);
-        rect(50, 300, 22, 90); //1
-        rect(95, 300, 22, 90); //2
-        rect(185, 300, 22, 90); //3
-        rect(230, 300, 22, 90); //4
-        rect(275, 300, 22, 90); //5
+        fill(60, 60, 60);
+        stroke(60, 60, 60);
+        rect(60, 250, 22, 80); //1
+        rect(105, 250, 22, 80); //2
+        rect(195, 250, 22, 80); //3
+        rect(240, 250, 22, 80); //4
+        rect(285, 250, 22, 80); //5
   
         noStroke();
         textSize(18);
-        fill(0, 102, 153);    
+        fill(60, 60, 60);    
     }
   }
 function setup() {
@@ -113,9 +105,21 @@ function setup() {
       i=0;
     }
     afft = new p5.FFT();
-    afft.setInput(amp3);
 }
+function draw() {
 
+    let waveform = afft.waveform(); 
+    beginShape();
+    strokeWeight(0.25);
+    stroke(60, 60, 60);
+    fill(204, 204, 255);
+    for (let i = 0; i < waveform.length; i++) {
+        let x = map(i, 0, waveform.length, 0, width);
+        let y = map(-height, -1, 5, waveform[i], 0);
+        vertex(x, y);
+    }
+    endShape();
+}
 function playNote(value) {
     if(noteValue = value) {
       console.log('success');
@@ -130,32 +134,29 @@ if(window.innerWidth > 992) {
 function mouseClicked() {
     let xPos = mouseX;
     if(xPos >= 250 && xPos <= 950){
-  
+    
     // you played a c!
     if(xPos > 250 && xPos < 350) {
       clear();
       setup();
       notePlayed = 'C';
       playNote(notePlayed);
-      console.log('You just played a ' + notePlayed);
-      text('You played a C', 500, 250);
-      fill(0, 0, 255);
-      ellipse(300, 225, 20, 20);
-      stroke(0, 0, 255);
-      line(285, 225, 315, 225);
+      text('You played a C', 520, 225);
+      afft.setInput(cmp3);
+      fill(60, 60, 60);
+      ellipse(300, 215, 20, 20);
+      stroke(60, 60, 60);
+      line(285, 215, 315, 215);
       cmp3.play();
     }
     // you played a d!
     else if(xPos > 350 && xPos < 450) {
       clear();
       setup();
-      notePlayed = 'E';
-      let fakeValue = 'D';
-      playNote(fakeValue);
-      console.log('You just played a ' + notePlayed);
-      text('You played a D', 500, 250);
-      fill(0, 0, 255);
-      ellipse(400, 213, 20, 20);
+      text('You played a D', 520, 225);
+      afft.setInput(dmp3);
+      fill(60, 60, 60);
+      ellipse(400, 203, 20, 20);
       dmp3.play();
     }
   
@@ -163,9 +164,10 @@ function mouseClicked() {
     if(xPos > 450 && xPos < 550) {
       clear();
       setup();
-      text('You played an E!', 500, 250);
-      fill(0, 0, 255);
-      ellipse(500, 200, 20, 20);
+      text('You played an E!', 520, 225);
+      afft.setInput(emp3);
+      fill(60, 60, 60);
+      ellipse(500, 190, 20, 20);
       emp3.play();
     }
   
@@ -173,9 +175,10 @@ function mouseClicked() {
     if(xPos > 550 && xPos < 650) {
       clear();
       setup();
-      text('You played an F!', 500, 250);
-      fill(0, 0, 255);
-      ellipse(600, 185, 20, 20);
+      text('You played an F!', 520, 225);
+      afft.setInput(fmp3);
+      fill(60, 60, 60);
+      ellipse(600, 175, 20, 20);
       fmp3.play();
     }
   
@@ -183,9 +186,10 @@ function mouseClicked() {
     if(xPos > 650 && xPos < 750) {
       clear();
       setup();
-      text('You played a G!', 500, 250);
-      fill(0, 0, 255);
-      ellipse(700, 170, 20, 20);
+      text('You played a G!', 520, 225);
+      afft.setInput(gmp3);
+      fill(60, 60, 60);
+      ellipse(700, 160, 20, 20);
       gmp3.play();
     }
   
@@ -193,9 +197,10 @@ function mouseClicked() {
     if(xPos > 750 && xPos < 850) {
       clear();
       setup();
-      text('You played an A!', 500, 250);
-      fill(0, 0, 255);
-      ellipse(800, 155, 20, 20);
+      text('You played an A!', 520, 225);
+      afft.setInput(amp3);
+      fill(60, 60, 60);
+      ellipse(800, 145, 20, 20);
       amp3.play();
     }
   
@@ -203,9 +208,10 @@ function mouseClicked() {
     if(xPos > 850 && xPos < 950) {
       clear();
       setup();
-      text('You played a B!', 500, 250);
-      fill(0, 0, 255);
-      ellipse(900, 140, 20, 20);
+      text('You played a B!', 520, 225);
+      afft.setInput(bmp3);
+      fill(60, 60, 60);
+      ellipse(900, 130, 20, 20);
       bmp3.play();
     }
   }
@@ -218,54 +224,54 @@ if(window.innerWidth < 992) {
         let display = mouseX;
        // text(display, 5, 10);
 
-        if(display > 15 && display < 60) {
+        if(display > 25 && display < 70) {
             text('You played a C!', 120, 55);
-            fill(0, 0, 255);
-            ellipse(35, 225, 20, 20);
-            stroke(0, 0, 255);
-            line(20, 225, 50, 225);
+            fill(60, 60, 60);
+            ellipse(45, 215, 15, 15);
+            stroke(60, 60, 60);
+            line(35, 215, 55, 215);
             cmp3.play();
         }
         
-        if(display > 60 && display < 105) {
+        if(display > 70 && display < 115) {
             text('You played a D!', 120, 55);
-            fill(0, 0, 255);
-            ellipse(85, 213, 20, 20);
+            fill(60, 60, 60);
+            ellipse(85, 213, 15, 15);
             dmp3.play();
         }
 
-        if(display > 105 && display < 150) {
+        if(display > 115 && display < 160) {
             text('You played an E!', 120, 55);
-            fill(0, 0, 225);
-            ellipse(130, 200, 20, 20);
+            fill(60, 60, 60);
+            ellipse(130, 190, 15, 15);
             emp3.play();
         }
 
-        if(display > 150 && display < 195) {
+        if(display > 160 && display < 205) {
             text('You played an F!', 120, 55);
-            fill(0, 0, 225);
-            ellipse(180, 185, 20, 20);
+            fill(60, 60, 60);
+            ellipse(180, 175, 15, 15);
             fmp3.play();
         }
 
-        if(display > 195 && display < 240) {
+        if(display > 205 && display < 250) {
             text('You played a G!', 120, 55);
-            fill(0, 0, 225);
-            ellipse(220, 170, 20, 20);
+            fill(60, 60, 60);
+            ellipse(220, 160, 15, 15);
             gmp3.play();
         }
 
-        if(display > 240 && display < 285) {
+        if(display > 250 && display < 295) {
             text('You played an A!', 120, 55);
-            fill(0, 0, 225);
-            ellipse(260, 155, 20, 20);
+            fill(60, 60, 60);
+            ellipse(260, 145, 15, 15);
             amp3.play();
         }
 
-        if(display > 285 && display < 330) {
+        if(display > 295 && display < 340) {
             text('You played a B!', 120, 55);
-            fill(0, 0, 255);
-            ellipse(330, 140, 20, 20);
+            fill(60, 60, 60);
+            ellipse(320, 130, 15, 15);
             bmp3.play();
         }
         }
